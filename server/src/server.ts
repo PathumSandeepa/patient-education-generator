2e609c55a203029e3ea1c24c8426dae826df7b20
PathumSandeepa <pathumsandeepa13@gmail.com>
2025-07-17 08:23:20 +0530

    [MEDGEMMA-01] feat: establish client-server communication via health-check

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,17 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-console.log(process.env.PORT);
+app.use(cors());
+app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-   res.send("Hello from the Express server!");
+app.get('/api/health-check', (req: Request, res: Response) => {
+  res.json({ message: 'Hello from the Express server! I am healthy!' });
 });
 
 app.listen(PORT, () => {
